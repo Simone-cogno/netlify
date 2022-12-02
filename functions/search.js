b2b3f53d649a3592ca294093aa4077737f6d7e30
Simone Cogno <simone.cogno@cleveron.ch>
2022-12-02 16:01:07 +0100

    [FEATURE] Adds search

--- functions/search.js
+++ functions/search.js
@@ -2,7 +2,7 @@ const qs = require('querystring');
 const yahooFinance = require('yahoo-finance2').default; // NOTE the .default
 const _ = require('lodash');
 
-const structure = {
+const quoteSummaryStructure = {
     incomeStatementHistory: {
         maxAge: "maxAge",
         endDate: "endDate",
@@ -28,8 +28,43 @@ const structure = {
         otherItems: "otherItems",
         netIncome: "netIncome",
         netIncomeApplicableToCommonShares: "netIncomeApplicableToCommonShares",
-    }
-}
+    },
+    "defaultKeyStatistics": {
+        "maxAge": "maxAge",
+        "priceHint": "priceHint",
+        "enterpriseValue": "enterpriseValue",
+        "forwardPE": "forwardPE",
+        "profitMargins": "profitMargins",
+        "floatShares": "floatShares",
+        "sharesOutstanding": "sharesOutstanding",
+        "sharesShort": "sharesShort",
+        "sharesShortPriorMonth": "sharesShortPriorMonth",
+        "sharesShortPreviousMonthDate": "sharesShortPreviousMonthDate",
+        "dateShortInterest": "dateShortInterest",
+        "sharesPercentSharesOut": "sharesPercentSharesOut",
+        "heldPercentInsiders": "heldPercentInsiders",
+        "heldPercentInstitutions": "heldPercentInstitutions",
+        "shortRatio": "shortRatio",
+        "shortPercentOfFloat": "shortPercentOfFloat",
+        "impliedSharesOutstanding": "impliedSharesOutstanding",
+        "category": "category",
+        "bookValue": "bookValue",
+        "priceToBook": "priceToBook",
+        "fundFamily": "fundFamily",
+        "legalType": "legalType",
+        "lastFiscalYearEnd": "lastFiscalYearEnd",
+        "nextFiscalYearEnd": "nextFiscalYearEnd",
+        "mostRecentQuarter": "mostRecentQuarter",
+        "netIncomeToCommon": "netIncomeToCommon",
+        "trailingEps": "trailingEps",
+        "pegRatio": "pegRatio",
+        "lastSplitFactor": "lastSplitFactor",
+        "enterpriseToRevenue": "enterpriseToRevenue",
+        "enterpriseToEbitda": "enterpriseToEbitda",
+        "52WeekChange": "52WeekChange",
+        "SandP52WeekChange": "SandP52WeekChange"
+      }
+};
 
 
 exports.handler = async (event) => {
@@ -42,19 +77,27 @@ exports.handler = async (event) => {
         };
     }
 
-    const quote = await yahooFinance.quote(query.ticker);
-
+    //https://github.com/gadicc/node-yahoo-finance2/blob/HEAD/docs/modules/quote.md
+    const quote = await yahooFinance.quote('INTC');
+    const quoteSummary = await yahooFinance.quoteSummary('INTC', { modules: [ "defaultKeyStatistics" ] });
+    const defaultKeyStatistics = quoteSummary.defaultKeyStatistics;
 
     console.log(quote);
     console.log(quote.trailingAnnualDividendYield * 100);
+    console.log(defaultKeyStatistics[quoteSummaryStructure.defaultKeyStatistics.pegRatio]);
+    console.log(defaultKeyStatistics[quoteSummaryStructure.defaultKeyStatistics.enterpriseToEbitda]);
 
     let dividendYield = ("" + quote.trailingAnnualDividendYield).replace('.', ',');
+    let pegRatio = defaultKeyStatistics[quoteSummaryStructure.defaultKeyStatistics.pegRatio];
+    let enterpriseToEbitda = defaultKeyStatistics[quoteSummaryStructure.defaultKeyStatistics.enterpriseToEbitda];
 
     return {
         statusCode: 200,
         body: JSON.stringify({
             ticket: query.ticker,
-            dividendYield
+            dividendYield,
+            pegRatio,
+            enterpriseToEbitda
         })
     };
 };
